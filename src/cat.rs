676c0fa72ddd3b6865468582118b240add3c1408
SpamixOfficial <spamixproducer@gmail.com>
2024-07-15 17:37:57 +0200

    Implemented more cat options

--- src/cat.rs
+++ src/cat.rs
@@ -1,10 +1,18 @@
 use std::{env::{args_os, Args}, fs};
-use taap;
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[clap(value_parser, num_args = 1.., value_delimiter = ' ')]
+    files: Vec<String>,
+    #[arg(short = 'A', long = "show-all", help = "equivalent to -vET, indicate all non-printed characters")]
+    show_all: bool
+}
 
 pub fn main() {
-    /*let mut argobject = taap::Argument::new("cat", "cat coreutil", "", "SpamixOfficial");
-    argobject.add_arg("FILE", "+", );
-    let parsed_args = argobject.parse_args(None); */
-    dbg!(args_os());
-    //dbg!(parsed_args); 
+    let cli = Cli::parse();
+    dbg!(&cli);
 }
